6571d7fab1f033c92c6b9667a3513dd7de63b88c
Orion777-cmd <abiy.biru78@gmail.com>
2023-12-01 23:31:30 +0300

    added some animation for the cells

--- Super-Tictactoe/src/App.tsx
+++ Super-Tictactoe/src/App.tsx
@@ -4,6 +4,8 @@ import BigXO from "./components/BigXO/bigXo.component";
 // import XO from "./components/XO/xo.component";
 
 
+// DO NOT FORGET TO ADD JOHN MAYERS SONG X 0 TO THE PROJECT
+
 function App() {
   
 
--- Super-Tictactoe/src/components/cell/cell.component.tsx
+++ Super-Tictactoe/src/components/cell/cell.component.tsx
@@ -1,8 +1,24 @@
 import "./cell.styles.css"
+import {useState} from "react";
+
+
+
 const Cell = () => {
+    const renderSign = () => {
+        setClicked(true);
+        if (sign === "X") {
+
+            setSign("O");
+        }
+        else {
+            setSign("X");
+        }
+    }
+    const [sign, setSign] = useState("X");
+    const [clicked, setClicked] = useState(false);
     return (
-        <div className="cell-container">
-            
+        <div className={`cell-container ${clicked? "transparent-container": ''}`} onClick={renderSign}>
+            {clicked && <div className={`render-${sign}`}></div>}
         </div>
     )
 }
