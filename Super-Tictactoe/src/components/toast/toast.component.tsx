de62d18f2aff8eb9106933a01c127b73517e08fe
Orion777-cmd <abiy.biru78@gmail.com>
2024-03-04 22:34:33 +0300

    add style with animation for the toast component

---
+++ Super-Tictactoe/src/components/toast/toast.component.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from "react";
+import "./toast.styles.scss";
+
+
+type ToastPopupPropType = {
+    children: string
+    status : string 
+}
+const ToastPopups:React.FC<ToastPopupPropType> = ({children, status}) => {
+    const [toast, setToast] = useState(false);
+
+    const TostAnimation = () => {
+        setToast(true);
+        setTimeout(() => {
+            setToast(false);
+        }, 3000);
+    }
+    return (
+        <>
+            <div className={`toast-container ${status} ${toast ? 'show' : ''}`}>
+                <div className="content-container">
+                    
+                    {children}
+                    <div className={`progress ${toast? 'animate': ''}`}>
+
+                    </div>
+                </div>
+            </div>
+            <button className="button" onClick={TostAnimation}>Toast</button>
+        </>
+        
+    )
+}
+
+export default ToastPopups;
