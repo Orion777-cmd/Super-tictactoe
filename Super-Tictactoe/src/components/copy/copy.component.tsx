3583150d06ce3cf437f0859e04df4c18833e6235
Orion777-cmd <abiy.biru78@gmail.com>
2024-03-04 22:53:59 +0300

    add logic and style part for copy component

--- Super-Tictactoe/src/components/copy/copy.component.tsx
+++ Super-Tictactoe/src/components/copy/copy.component.tsx
@@ -1,8 +1,29 @@
 import "./copy.styles.css";
+import {useState} from "react";
+import { FaRegCopy } from "react-icons/fa";
 
-const Copy = () => {
+type CopyPropsTypes = {
+    text : string
+}
+const Copy: React.FC<CopyPropsTypes> = ({text}) => {
+
+    const [isCopied, setIsCopied] = useState<boolean>(false);
+
+    const handlecopy = () => {
+        navigator.clipboard.writeText(text);
+        setIsCopied(true);
+    }
+
+    setTimeout(()  => {
+        setIsCopied(false);
+    }, 2000);
     return (
-
+        <div className="button-container">
+            <button onClick={handlecopy}>
+                <FaRegCopy size={20}/>
+                {isCopied? 'copied!': 'copy'}
+            </button>
+        </div>
     )
 }
 
